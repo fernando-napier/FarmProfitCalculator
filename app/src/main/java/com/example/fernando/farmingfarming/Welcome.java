599e3efed836454db1e828a6c76f74107d243432
Fernando <fernando.napier@gmail.com>
2015-08-30 12:25:43 -0500

    the update includes a few classes.
    - CenterHorizontalView is for the activity_welcome.xml in for the ImageView layouts
    
    - Added a few onclicklisteners for the images in the activity_welcome.xml file
    
    Next commit:
       1) rid the project of the profit class and activity
       2) create graphs that display the data on the CornProfit Screen

--- app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
+++ app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
@@ -1,13 +1,13 @@
 package com.example.fernando.farmingfarming;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
@@ -16,36 +16,151 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        ImageView cornImage = (ImageView) findViewById(R.id.welcomeCorn);
+        final ImageView cornImage = (ImageView) findViewById(R.id.welcomeCorn);
+        final ImageView soybeanImage = (ImageView) findViewById(R.id.welcomeSoybean);
+        final ImageView cottonImage = (ImageView) findViewById(R.id.welcomeCotton);
+        final TextView text = (TextView) findViewById(R.id.welcomeSampleText);
+        final CenterHorizontalView imageScroll = (CenterHorizontalView) findViewById(R.id.welcomeScrollImages);
+        imageScroll.setCurrentItemAndCenter(1);
 
-        cornImage.setOnClickListener(new View.OnClickListener(){
-           public void onClick(View v){
-               Log.d("Welcome Activity"," Button Clicked ");
-               startActivity(new Intent(Welcome.this, Profit.class));
+
+        cornImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                Log.d("corn ID", "" + cornImage.getId());
+                if (imageScroll.getCurrentActiveItem() == 0) {
+                    text.setText("Corn Selected");
+                    AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
+                    alert.setTitle("Corn Chosen");
+                    alert.setMessage("Choose an option!");
+                    alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Region Average",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Customize",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_POSITIVE, "Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.show();
+
+                } else if (imageScroll.getCurrentActiveItem() == 1) {
+                    soybeanImage.performClick();
+
+                } else if (imageScroll.getCurrentActiveItem() == 2) {
+                    cottonImage.performClick();
+
+                }
 
             }
         });
+        soybeanImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                Log.d("soybean ID", "" + soybeanImage.getId());
+
+                if (imageScroll.getCurrentActiveItem() == 0) {
+                    cornImage.performClick();
+
+                } else if (imageScroll.getCurrentActiveItem() == 1) {
+                    text.setText("Soybean Selected");
+                    AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
+                    alert.setTitle("Soybean Chosen");
+                    alert.setMessage("Choose an option!");
+                    alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Region Average",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Customize",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_POSITIVE, "Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.show();
+
+                } else if (imageScroll.getCurrentActiveItem() == 2) {
+                    cottonImage.performClick();
+
+                }
+
+
+
+            }
+
+        });
+        cottonImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                Log.d("cotton ID", "" + cottonImage.getId());
+
+                if (imageScroll.getCurrentActiveItem() == 0) {
+                    cornImage.performClick();
+
+                } else if (imageScroll.getCurrentActiveItem() == 1) {
+                    soybeanImage.performClick();
+
+                } else if (imageScroll.getCurrentActiveItem() == 2) {
+                    text.setText("Cotton Selected");
+
+                    AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
+                    alert.setTitle("Cotton Chosen");
+                    alert.setMessage("Choose an option!");
+                    alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Region Average",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Customize",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.setButton(AlertDialog.BUTTON_POSITIVE, "Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alert.show();
+
+                }
+
+
+            }
+
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
-        return true;
+    protected synchronized void buildGoogleApiClient(){
+       // GoogleApiClient apiClient = new GoogleApiClient.Builder(this);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
