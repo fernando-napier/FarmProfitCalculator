3ffdcfdaf46d25dcda9fea78557456cc9bc40c72
Fernando <fernando.napier@gmail.com>
2015-08-31 12:44:00 -0500

    the update includes a few classes.
    - CenterHorizontalView is for the activity_welcome.xml in for the ImageView layouts
    
    - Added a few onclicklisteners for the images in the activity_welcome.xml file
    
    Next commit:
       1) rid the project of the profit class and activity
       2) create graphs that display the data on the CornProfit Screen

--- app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
+++ app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
@@ -2,14 +2,28 @@ package com.example.fernando.farmingfarming;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Welcome extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class Welcome extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static int REQUEST_CODE_RECOVER_PLAY_SERVICES = 200;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,7 +120,6 @@ public class Welcome extends AppCompatActivity {
                 }
 
 
-
             }
 
         });
@@ -117,13 +130,7 @@ public class Welcome extends AppCompatActivity {
                 Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
                 Log.d("cotton ID", "" + cottonImage.getId());
 
-                if (imageScroll.getCurrentActiveItem() == 0) {
-                    cornImage.performClick();
-
-                } else if (imageScroll.getCurrentActiveItem() == 1) {
-                    soybeanImage.performClick();
-
-                } else if (imageScroll.getCurrentActiveItem() == 2) {
+                if (imageScroll.getCurrentActiveItem() == 2) {
                     text.setText("Cotton Selected");
 
                     AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
@@ -159,8 +166,100 @@ public class Welcome extends AppCompatActivity {
 
     }
 
-    protected synchronized void buildGoogleApiClient(){
-       // GoogleApiClient apiClient = new GoogleApiClient.Builder(this);
+    private boolean checkGooglePlayServices() {
+
+        int checkGooglePlayServices = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (checkGooglePlayServices != ConnectionResult.SUCCESS) {
+            /*
+            * google play services is missing or update is required
+			*  return code could be
+			* SUCCESS,
+			* SERVICE_MISSING, SERVICE_VERSION_UPDATE_REQUIRED,
+			* SERVICE_DISABLED, SERVICE_INVALID.
+			*/
+            GooglePlayServicesUtil.getErrorDialog(checkGooglePlayServices,
+                    this, REQUEST_CODE_RECOVER_PLAY_SERVICES).show();
+
+            return false;
+        }
+
+        return true;
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_RECOVER_PLAY_SERVICES) {
+
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Google Play Services must be installed.",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+    }
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+
+            Toast.makeText(this, "Latitude:" + mLastLocation.getLatitude() + ", Longitude:" + mLastLocation.getLongitude(), Toast.LENGTH_LONG).show();
+
+
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        Log.d("connnection","suspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        Log.d("connnection","failed");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
 
 }
