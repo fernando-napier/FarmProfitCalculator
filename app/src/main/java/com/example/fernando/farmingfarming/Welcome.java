f4ea28158e2c583fd076ddc36e420653091452d4
Fernando <fernandonapier@gmail.com>
2015-08-31 13:44:48 -0500

    added scrollview to the welcome screen.

--- app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
+++ app/src/main/java/com/example/fernando/farmingfarming/Welcome.java
@@ -2,6 +2,10 @@ package com.example.fernando.farmingfarming;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,7 +13,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class Welcome extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class Welcome extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    GoogleApiClient apiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +39,33 @@ public class Welcome extends AppCompatActivity {
         imageScroll.setCurrentItemAndCenter(1);
 
 
+
+        apiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        apiClient.connect();
+
         cornImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
-                Log.d("corn ID", "" + cornImage.getId());
+                //Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                //Log.d("corn ID", "" + cornImage.getId());
                 if (imageScroll.getCurrentActiveItem() == 0) {
-                    text.setText("Corn Selected");
+
                     AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
                     alert.setTitle("Corn Chosen");
                     alert.setMessage("Choose an option!");
                     alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Region Average",
                             new DialogInterface.OnClickListener() {
                                 public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
+                                    Intent i = new Intent(Welcome.this, CornProfit.class);
+                                    i.putExtra("region", 0);
+                                    i.putExtra("crop", 0);
+                                    startActivity(i);
                                 }
                             });
                     alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Customize",
@@ -55,28 +82,17 @@ public class Welcome extends AppCompatActivity {
                             });
                     alert.show();
 
-                } else if (imageScroll.getCurrentActiveItem() == 1) {
-                    soybeanImage.performClick();
-
-                } else if (imageScroll.getCurrentActiveItem() == 2) {
-                    cottonImage.performClick();
-
                 }
-
             }
         });
         soybeanImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
-                Log.d("soybean ID", "" + soybeanImage.getId());
+                //Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                //Log.d("soybean ID", "" + soybeanImage.getId());
 
-                if (imageScroll.getCurrentActiveItem() == 0) {
-                    cornImage.performClick();
-
-                } else if (imageScroll.getCurrentActiveItem() == 1) {
-                    text.setText("Soybean Selected");
+                if (imageScroll.getCurrentActiveItem() == 1) {
                     AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
                     alert.setTitle("Soybean Chosen");
                     alert.setMessage("Choose an option!");
@@ -100,11 +116,7 @@ public class Welcome extends AppCompatActivity {
                             });
                     alert.show();
 
-                } else if (imageScroll.getCurrentActiveItem() == 2) {
-                    cottonImage.performClick();
-
                 }
-
 
 
             }
@@ -114,17 +126,12 @@ public class Welcome extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
-                Log.d("cotton ID", "" + cottonImage.getId());
+                // Log.d("current active item", "" + imageScroll.getCurrentActiveItem());
+                // Log.d("cotton ID", "" + cottonImage.getId());
 
-                if (imageScroll.getCurrentActiveItem() == 0) {
-                    cornImage.performClick();
 
-                } else if (imageScroll.getCurrentActiveItem() == 1) {
-                    soybeanImage.performClick();
+                if (imageScroll.getCurrentActiveItem() == 2) {
 
-                } else if (imageScroll.getCurrentActiveItem() == 2) {
-                    text.setText("Cotton Selected");
 
                     AlertDialog alert = new AlertDialog.Builder(Welcome.this).create();
                     alert.setTitle("Cotton Chosen");
@@ -159,8 +166,52 @@ public class Welcome extends AppCompatActivity {
 
     }
 
-    protected synchronized void buildGoogleApiClient(){
-       // GoogleApiClient apiClient = new GoogleApiClient.Builder(this);
+    /**
+     * this method is made to show the last known location of the user
+     */
+
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        double latitude;
+        double longitude;
+        List<Address> addresses = null;
+        TextView text = (TextView) findViewById(R.id.welcomeSampleText);
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+        if (lastLocation != null) {
+            latitude = lastLocation.getLatitude();
+            longitude = lastLocation.getLongitude();
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (addresses == null) {
+
+                    text.setText("Please Input Region");
+
+                } else {
+
+                    text.setText("State: " + addresses.get(0).getAdminArea());
+                }
+            }
+
+
+        }
+
+
     }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
